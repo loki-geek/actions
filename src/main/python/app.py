8d6722ac072dbaee3779758a2156b19c6fd60838
loki-geek <136673117+loki-geek@users.noreply.github.com>
2024-02-26 15:00:51 +0530

    Updated app.py with docstring

--- src/main/python/app.py
+++ src/main/python/app.py
@@ -1,10 +1,11 @@
-
+"""Module providing a function of app name"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    """Function printing Hello world."""
     return "Hello, World!"
 
 if __name__ == "__main__":
